6e78a6dd7b0ade5f0f4b2890b4b9cd7997fab87b
ynFatal <63413763@qq.com>
2020-06-24 05:44:31 +0800

    edit class name to express microservice provider of simple characteristic

--- openfeign-provider-example/src/main/java/com/example/controller/SimpleProviderController.java
+++ openfeign-provider-example/src/main/java/com/example/controller/SimpleProviderController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 测试 OpenFeign 的简单特性
  * @author Fatal
  * @date 2020/6/23
  */
 @RestController
-public class ProviderController {
+public class SimpleProviderController {
 
     @GetMapping("/hello")
     public String hello() {
