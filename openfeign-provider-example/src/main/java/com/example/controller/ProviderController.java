167cb9356c8fd5931e18e628b905488ecc1d9618
ynFatal <63413763@qq.com>
2020-06-24 05:19:43 +0800

    edit author name

--- openfeign-provider-example/src/main/java/com/example/controller/ProviderController.java
+++ openfeign-provider-example/src/main/java/com/example/controller/ProviderController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Belo-betty
+ * @author Fatal
  * @date 2020/6/23
  */
 @RestController
