2aaa0de53c228e5866c88c4f9a573f2fb98f0e26
ynFatal <634137063@qq.com>
2020-06-23 09:01:29 +0800

    init openfeign-provider-example

---
+++ openfeign-provider-example/src/main/java/com/example/OpenfeignProviderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignProviderExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignProviderExampleApplication.class, args);
+    }
+
+}
