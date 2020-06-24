a9dfda2ca7bb4c2f97f1562937dd74d1d93cdbf9
ynFatal <634137063@qq.com>
2020-06-24 09:01:31 +0800

    add test and note for FeignClientSpecification

--- openfeign-consumer-example/src/main/java/com/example/OpenfeignConsumerExampleApplication.java
+++ openfeign-consumer-example/src/main/java/com/example/OpenfeignConsumerExampleApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import java.util.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+// 开启 FeignClient 注册
 @EnableFeignClients
 public class OpenfeignConsumerExampleApplication {
 
@@ -14,4 +19,13 @@ public class OpenfeignConsumerExampleApplication {
         SpringApplication.run(OpenfeignConsumerExampleApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            System.out.println("看看当前 Spring 容器中的 Bean: ");
+            Stream.of(context.getBeanDefinitionNames())
+                .sorted()
+                .forEach(System.out::println);
+        };
+    }
 }
