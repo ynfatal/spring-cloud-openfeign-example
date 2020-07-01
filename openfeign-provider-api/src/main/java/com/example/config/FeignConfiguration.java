4709a9ce53e8961e6078b3ca78d7286bdf09c1c0
ynFatal <63413763@qq.com>
2020-07-02 05:36:44 +0800

    edit notes.

--- openfeign-provider-api/src/main/java/com/example/config/FeignConfiguration.java
+++ openfeign-provider-api/src/main/java/com/example/config/FeignConfiguration.java
@@ -45,22 +45,31 @@ import java.util.List;
  *  在类路径上，则使用 FeignBlockingLoadBalancerClient。如果它们都不在类路径上，就使用默认的 feign client。
  * 3. FeignClientsConfiguration 也被标注了 @Configuration，它里边也配置了很多 Bean，为什么这个配置组件包括它包含的 Bean 没有被
  * Spring 容器管理，答案是 @ComponentScan 是扫描不到这个配置组件，所以它没有托管 Spring 容器。
- * 4. 同时使用自定义局部配置和自定义全局配置的话，配置行为如下：
+ * 4. 同时使用自定义局部配置和自定义全局配置的话
+ * ===========  方式一  ===========
+ * 配置行为如下：
  * - 自定义全局：使用 org.springframework.cloud.openfeign.EnableFeignClients#defaultConfiguration() 指定自定义全局配置类，该配置类不需要标注为配置组件
  * - 自定义局部：使用 org.springframework.cloud.openfeign.FeignClient#configuration() 指定自定义局部配置类，该配置类不需要标注为配置组件
  * 注意事项：
  * - 相同类型的 Bean，在自定义局部配置类和自定义全局配置类中不能重复，否则会造成局部的 Bean 无法实例化，这样局部就没作用了
  * - 在自义局部配置类给该 Bean 添加标注 @Primary，以表示该类型的 Bean 自定义局部配置高于自定义全局配置。
- * - 如果是默认全局与其中一个自定义配置搭配，那么随便玩，要是怕出问题也可以以当前这个最严谨的方式配置，也就是两点注意事项。
+ * 如果是默认全局与其中一个自定义配置搭配，那么随便玩，要是怕出问题也可以以当前这个最严谨的方式配置，也就是两点注意事项。
  * 理解：
  * Spring 容器中，相同类型相同名字的Bean是不可能存在的，而在子上下文之间，可以这样配置，但是只会实例化第一个，也就是全局的那个。debug 一下你就知道了。
  * 扩展：
- * 其实这里的自定义配置没必要标注 @Configuration，因为这里的配置是给字上下文用的，标注后，在父上下文中又没用。
+ * 其实这里的自定义配置没必要标注 @Configuration，因为这里的配置是给子上下文用的，标注后，在父上下文中又没用。
  * 如果在父上下文中需要用到，可以添加标注，否则，去掉吧。
+ * ===========  方式二  ===========
+ * 配置行为如下：
+ * - 自定义全局：不使用 org.springframework.cloud.openfeign.EnableFeignClients#defaultConfiguration() 指定自定义全局配置类，该配置类需要标注为配置组件
+ * 放在 ComponentScan 能扫描的地方即可
+ * - 自定义局部：同方式一
+ * 这种方式不需要考虑方式一的注意事项，但是建议考虑比较好。
+ * 理解：建议使用第一种方式，不标注 @Configuration，要不然在父上下文和子上下文中都有这套 Bean，但是父上下文的又没有被用到，实属多余。
  * @author Fatal
  * @date 2020/6/28 8:47
  */
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 @AllArgsConstructor
 public class FeignConfiguration {
 
